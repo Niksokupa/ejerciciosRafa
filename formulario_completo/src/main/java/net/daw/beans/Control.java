597d253fae95fe5a6e6b464d22692cad1b70ddf8
Unknown <llius619@gmail.com>
2018-09-27 10:37:51 +0200

    Mejoras formulario_completo

--- formulario_completo/src/main/java/net/daw/beans/Control.java
+++ formulario_completo/src/main/java/net/daw/beans/Control.java
@@ -59,69 +59,103 @@ public class Control extends HttpServlet {
 
 		ArrayList<Fila> tablaJSON = new ArrayList<Fila>();
 		ArrayList filas;
+		
+		ArrayList<ArrayList> errores = new ArrayList<ArrayList>();
+		ArrayList<String> lineaerrores;
 
 		StringBuilder sb = new StringBuilder();
 
 		if (request.getParameter("ancho").equals("")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Ancho vacio");
-			sb.append("Error: Ancho vacio");
-		} else if (request.getParameter("alto").equals("")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Alto vacio");
-			sb.append("Error: Alto vacio");
-		
-		} else if (!request.getParameter("alto").matches("^[0-9][0-9]*$")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce caracteres numericos en alto ");
-			sb.append("Error: Introduce caracteres numericos en alto ");
-		} else if (!request.getParameter("ancho").matches("^[0-9][0-9]*$")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce caracteres numericos en ancho");
-			sb.append("Error: Introduce caracteres numericos en ancho ");
-		} else if (!request.getParameter("ancho").matches("^([0-9][0-9]?|100)$")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce un valor menor que 101 en ancho");
-			sb.append("Error: Introduce un valor menor que 101 en ancho");
-		} else if (!request.getParameter("alto").matches("^([0-9][0-9]?|100)$")) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce un valor menor que 101 en alto");
-			sb.append("Error: Introduce un valor menor que 101 en alto");
-		} else if (Integer.parseInt(request.getParameter("ancho")) == 0
-				|| Integer.parseInt(request.getParameter("ancho")) < 0) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce un valor mayor que 0 en ancho");
-		} else if (Integer.parseInt(request.getParameter("alto")) == 0
-				|| Integer.parseInt(request.getParameter("alto")) < 0) {
-			response.setStatus(404);
-			response.getWriter().write("Error: Introduce un valor mayor que 0 en alto");
-		} else {
-			System.out.print(sb);
-			for (int i = 1; i <= Integer.parseInt(request.getParameter("alto")); i++) {
-
-				filas = new ArrayList<Celdas>();
-
-				fila = new Fila();
-
-				for (int j = 1; j <= Integer.parseInt(request.getParameter("ancho")); j++) {
-					celdas = new Celdas();
-					celdas.setI(i);
-					celdas.setJ(j);
-					celdas.setR(j * i);
-
-					filas.add(celdas);
-
-				}
-
-				fila.setA(filas);
-				fila.setI(i);
-				tablaJSON.add(fila);
-			}
-			Gson gSon = new Gson();
-			String str = gSon.toJson(tablaJSON);
-
-			response.getWriter().append(str);
+			lineaerrores = new ArrayList<String>();
+			lineaerrores.add("Error: Ancho vacio");
+			errores.add(lineaerrores);
 		}
+		if (request.getParameter("alto").equals("")) {
+			lineaerrores = new ArrayList<String>();
+			lineaerrores.add("Error: Alto vacio");
+			errores.add(lineaerrores);
+		}
+		if (!request.getParameter("alto").matches("^[0-9][0-9]*$")) {
+			lineaerrores = new ArrayList<String>();
+			lineaerrores.add("Error: Introduce caracteres numericos o positivos en alto");
+			errores.add(lineaerrores);
+		} 
+		if (!request.getParameter("ancho").matches("^[0-9][0-9]*$")) {
+			lineaerrores = new ArrayList<String>();
+			lineaerrores.add("Error: Introduce caracteres numericos o positivos en ancho");
+			errores.add(lineaerrores);
+		}
+		if(errores.isEmpty() == false ) {
+			response.setStatus(404);
+			Gson gSon = new Gson();
+			String str = gSon.toJson(errores);
+			response.getWriter().write(str);
+		}else {
+			//SI LO QUE HE INTRODUCIDO SON NUMEROS ENTRARA AQUI
+			
+			if (!request.getParameter("ancho").matches("^([1-9][0-9]?|100)$")) {
+				if (Integer.parseInt(request.getParameter("ancho")) == 0
+						|| Integer.parseInt(request.getParameter("ancho")) < 0) {
+					lineaerrores = new ArrayList<String>();
+					lineaerrores.add("Error: Introduce un valor mayor que 0 en ancho");
+					errores.add(lineaerrores);
+				}else {
+					lineaerrores = new ArrayList<String>();
+					lineaerrores.add("Error: Introduce un valor menor que 101 en ancho");
+					errores.add(lineaerrores);						
+				}
+			}
+			if (!request.getParameter("alto").matches("^([1-9][0-9]?|100)$")) {
+				if (Integer.parseInt(request.getParameter("alto")) == 0
+						|| Integer.parseInt(request.getParameter("alto")) < 0) {
+					lineaerrores = new ArrayList<String>();
+					lineaerrores.add("Error: Introduce un valor mayor que 0 en alto");
+					errores.add(lineaerrores);
+				}else {
+					lineaerrores = new ArrayList<String>();
+					lineaerrores.add("Error: Introduce un valor menor que 101 en alto");
+					errores.add(lineaerrores);						
+				}
+			}
+			if(errores.isEmpty() == false ) {
+				response.setStatus(404);
+				Gson gSon = new Gson();
+				String str = gSon.toJson(errores);
+				response.getWriter().write(str);
+				
+			}else {
+				
+				//SI HA PASADO LAS VALIDACIONES DE ARRIBA QUIERE DECIR QUE LO QUE HE INTRODUCIDO SIN NUMEROS
+				//CORRECTOS ASI QUE CREARA LA TABLA
+				
+				for (int i = 1; i <= Integer.parseInt(request.getParameter("alto")); i++) {
+
+					filas = new ArrayList<Celdas>();
+
+					fila = new Fila();
+
+					for (int j = 1; j <= Integer.parseInt(request.getParameter("ancho")); j++) {
+						celdas = new Celdas();
+						celdas.setI(i);
+						celdas.setJ(j);
+						celdas.setR(j * i);
+
+						filas.add(celdas);
+
+					}
+
+					fila.setA(filas);
+					fila.setI(i);
+					tablaJSON.add(fila);
+				}
+				Gson gSon = new Gson();
+				String str = gSon.toJson(tablaJSON);
+
+				response.getWriter().append(str);
+	
+			}
+						}
+	
 	}
 
 }
