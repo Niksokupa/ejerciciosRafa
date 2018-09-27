0d0f6e5875e0d1bc62faba2f5bf3cb7f51568079
Unknown <llius619@gmail.com>
2018-09-27 08:54:32 +0200

    Cliente acabado

--- formulario_completo/src/main/java/net/daw/beans/Control.java
+++ formulario_completo/src/main/java/net/daw/beans/Control.java
@@ -19,67 +19,109 @@ import net.daw.clases.Fila;
  */
 public class Control extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Control() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		processRequest(request,response);
-		
+	public Control() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
+
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json");
-            
-            Fila fila;
-            Celdas celdas;
-            
-            ArrayList<Fila> tablaJSON = new ArrayList<Fila>();
-            ArrayList filas;
-            
-            for(int i = 1; i <= Integer.parseInt(request.getParameter("alto")); i++){
-             
-                filas = new ArrayList<Celdas>();
-                
-                fila = new Fila();
-              
-                for(int j = 1; j <= Integer.parseInt(request.getParameter("ancho")); j++){
-                    celdas = new Celdas();
-                    celdas.setI(i);
-                    celdas.setJ(j);
-                    celdas.setR(j*i);
-                    
-                    filas.add(celdas);
-                    
-                }
-                
-                fila.setA(filas);
-                fila.setI(i);
-                tablaJSON.add(fila);
-            }
-            Gson gSon = new Gson();
-            String str = gSon.toJson(tablaJSON);
-            
-            response.getWriter().append(str);
-    }
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+
+		Fila fila;
+		Celdas celdas;
+
+		ArrayList<Fila> tablaJSON = new ArrayList<Fila>();
+		ArrayList filas;
+
+		StringBuilder sb = new StringBuilder();
+
+		if (request.getParameter("ancho").equals("")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Ancho vacio");
+			sb.append("Error: Ancho vacio");
+		} else if (request.getParameter("alto").equals("")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Alto vacio");
+			sb.append("Error: Alto vacio");
+		
+		} else if (!request.getParameter("alto").matches("^[0-9][0-9]*$")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce caracteres numericos en alto ");
+			sb.append("Error: Introduce caracteres numericos en alto ");
+		} else if (!request.getParameter("ancho").matches("^[0-9][0-9]*$")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce caracteres numericos en ancho");
+			sb.append("Error: Introduce caracteres numericos en ancho ");
+		} else if (!request.getParameter("ancho").matches("^([0-9][0-9]?|100)$")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce un valor menor que 101 en ancho");
+			sb.append("Error: Introduce un valor menor que 101 en ancho");
+		} else if (!request.getParameter("alto").matches("^([0-9][0-9]?|100)$")) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce un valor menor que 101 en alto");
+			sb.append("Error: Introduce un valor menor que 101 en alto");
+		} else if (Integer.parseInt(request.getParameter("ancho")) == 0
+				|| Integer.parseInt(request.getParameter("ancho")) < 0) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce un valor mayor que 0 en ancho");
+		} else if (Integer.parseInt(request.getParameter("alto")) == 0
+				|| Integer.parseInt(request.getParameter("alto")) < 0) {
+			response.setStatus(404);
+			response.getWriter().write("Error: Introduce un valor mayor que 0 en alto");
+		} else {
+			System.out.print(sb);
+			for (int i = 1; i <= Integer.parseInt(request.getParameter("alto")); i++) {
+
+				filas = new ArrayList<Celdas>();
+
+				fila = new Fila();
+
+				for (int j = 1; j <= Integer.parseInt(request.getParameter("ancho")); j++) {
+					celdas = new Celdas();
+					celdas.setI(i);
+					celdas.setJ(j);
+					celdas.setR(j * i);
+
+					filas.add(celdas);
+
+				}
+
+				fila.setA(filas);
+				fila.setI(i);
+				tablaJSON.add(fila);
+			}
+			Gson gSon = new Gson();
+			String str = gSon.toJson(tablaJSON);
+
+			response.getWriter().append(str);
+		}
+	}
 
 }
