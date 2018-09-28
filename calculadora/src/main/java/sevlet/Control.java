5cc2887074dfad96dc7ce4bdc5b4132a2ca5c7fc
llius <llius619@gmail.com>
2018-09-28 09:33:48 +0200

    Calculadora acabado.

--- calculadora/src/main/java/sevlet/Control.java
+++ calculadora/src/main/java/sevlet/Control.java
@@ -50,14 +50,14 @@ public class Control extends HttpServlet {
 			lineaerrores.add("Error: segundo vacio");
 			errores.add(lineaerrores);
 		}
-		if (!request.getParameter("segundo").matches("^[0-9][0-9]*$") && !request.getParameter("segundo").equals("")) {
+		if (!request.getParameter("segundo").matches("^-?([1-9][0-9]?|100)$") && !request.getParameter("segundo").equals("")) {
 			lineaerrores = new ArrayList<String>();
-			lineaerrores.add("Error: Introduce caracteres numericos o positivos en segundo");
+			lineaerrores.add("Error: Introduce caracteres numericos,numeros enteros entre -100 y 100 o distinto de 0 en segundo");
 			errores.add(lineaerrores);
 		}
-		if (!request.getParameter("primero").matches("^[0-9][0-9]*$") && !request.getParameter("primero").equals("")) {
+		if (!request.getParameter("primero").matches("^-?([1-9][0-9]?|100)$") && !request.getParameter("primero").equals("")) {
 			lineaerrores = new ArrayList<String>();
-			lineaerrores.add("Error: Introduce caracteres numericos o positivos en primero");
+			lineaerrores.add("Error: Introduce caracteres numericos,numeros enteros entre 100 y -100 o distinto de 0 en primero");
 			errores.add(lineaerrores);
 		}
 		if (errores.isEmpty() == false) {
@@ -66,91 +66,151 @@ public class Control extends HttpServlet {
 			String str = gSon.toJson(errores);
 			response.getWriter().write(str);
 		} else {
-			// SI LO QUE HE INTRODUCIDO SON NUMEROS ENTRARA AQUI
+			// SI HA PASADO LAS VALIDACIONES DE ARRIBA QUIERE DECIR QUE LO QUE HE
+			// INTRODUCIDO SIN NUMEROS
+			// CORRECTOS ASI QUE CREARA LA TABLA
+			Gson gSon = new Gson();
+			String str = "";
+			
+			Double primero = Double.parseDouble(request.getParameter("primero"));
+			Double segundo = Double.parseDouble(request.getParameter("segundo"));
 
-			if (!request.getParameter("primero").matches("^([1-9][0-9]?|100)$")) {
-				if (Integer.parseInt(request.getParameter("primero")) == 0
-						|| Integer.parseInt(request.getParameter("primero")) < 0) {
-					lineaerrores = new ArrayList<String>();
-					lineaerrores.add("Error: Introduce un valor mayor que 0 en primero");
-					errores.add(lineaerrores);
-				} else {
-					lineaerrores = new ArrayList<String>();
-					lineaerrores.add("Error: Introduce un valor menor que 101 en primero");
-					errores.add(lineaerrores);
-				}
-			}
-			if (!request.getParameter("segundo").matches("^([1-9][0-9]?|100)$")) {
-				if (Integer.parseInt(request.getParameter("segundo")) == 0
-						|| Integer.parseInt(request.getParameter("segundo")) < 0) {
-					lineaerrores = new ArrayList<String>();
-					lineaerrores.add("Error: Introduce un valor mayor que 0 en segundo");
-					errores.add(lineaerrores);
-				} else {
-					lineaerrores = new ArrayList<String>();
-					lineaerrores.add("Error: Introduce un valor menor que 101 en segundo");
-					errores.add(lineaerrores);
-				}
-			}
-			if (errores.isEmpty() == false) {
+			Double resultado = 0.0;
+
+			BigDecimal value = new BigDecimal(0);
+
+			arrayResultado = new ArrayList();
+
+			switch (accion) {
+			case "Suma":
+				resultado = primero + segundo;
+				arrayResultado.add(resultado);
+				str = gSon.toJson(arrayResultado);
+				response.getWriter().append(str);
+				break;
+			case "Resta":
+				resultado = primero - segundo;
+				arrayResultado.add(resultado);
+				str = gSon.toJson(arrayResultado);
+				response.getWriter().append(str);
+				break;
+			case "Multiplicacion":
+				resultado = primero * segundo;
+				arrayResultado.add(resultado);
+				str = gSon.toJson(arrayResultado);
+				response.getWriter().append(str);
+				break;
+			case "Division":
+				resultado = primero / segundo;
+				arrayResultado.add(resultado);
+				str = gSon.toJson(arrayResultado);
+				response.getWriter().append(str);
+				break;
+			default:
 				response.setStatus(404);
-				Gson gSon = new Gson();
-				String str = gSon.toJson(errores);
+				lineaerrores = new ArrayList<String>();
+				lineaerrores.add("Error: Selecciona una accion");
+				errores.add(lineaerrores);
+				str = gSon.toJson(errores);
 				response.getWriter().write(str);
-
-			} else {
-
-				// SI HA PASADO LAS VALIDACIONES DE ARRIBA QUIERE DECIR QUE LO QUE HE
-				// INTRODUCIDO SIN NUMEROS
-				// CORRECTOS ASI QUE CREARA LA TABLA
-				Gson gSon = new Gson();
-				String str = "";
-				
-				Double primero = Double.parseDouble(request.getParameter("primero"));
-				Double segundo = Double.parseDouble(request.getParameter("segundo"));
-
-				Double resultado = 0.0;
-
-				BigDecimal value = new BigDecimal(0);
-
-				arrayResultado = new ArrayList<>();
-
-				switch (accion) {
-				case "Suma":
-					resultado = primero + segundo;
-					arrayResultado.add(resultado);
-					str = gSon.toJson(arrayResultado);
-					response.getWriter().append(str);
-					break;
-				case "Resta":
-					resultado = primero - segundo;
-					arrayResultado.add(resultado);
-					str = gSon.toJson(arrayResultado);
-					response.getWriter().append(str);
-					break;
-				case "Multiplicacion":
-					resultado = primero * segundo;
-					arrayResultado.add(resultado);
-					str = gSon.toJson(arrayResultado);
-					response.getWriter().append(str);
-					break;
-				case "Division":
-					resultado = primero / segundo;
-					arrayResultado.add(resultado);
-					str = gSon.toJson(arrayResultado);
-					response.getWriter().append(str);
-					break;
-				default:
-					response.setStatus(404);
-					lineaerrores = new ArrayList<String>();
-					lineaerrores.add("Error: Selecciona una accion");
-					errores.add(lineaerrores);
-					str = gSon.toJson(errores);
-					response.getWriter().write(str);
-					break;
-				}
-			}
+				break;
 		}
+		}
+		
+		
+		
+		
+			
+		
+		
+		
+		
+//		else {
+//			// SI LO QUE HE INTRODUCIDO SON NUMEROS ENTRARA AQUI
+//
+//			if (!request.getParameter("primero").matches("^([1-9][0-9]?|100)$")) {
+//				if (Integer.parseInt(request.getParameter("primero")) == 0
+//						|| Integer.parseInt(request.getParameter("primero")) < 0) {
+//					lineaerrores = new ArrayList<String>();
+//					lineaerrores.add("Error: Introduce un valor mayor que 0 en primero");
+//					errores.add(lineaerrores);
+//				} else {
+//					lineaerrores = new ArrayList<String>();
+//					lineaerrores.add("Error: Introduce un valor menor que 101 en primero");
+//					errores.add(lineaerrores);
+//				}
+//			}
+//			if (!request.getParameter("segundo").matches("^([1-9][0-9]?|100)$")) {
+//				if (Integer.parseInt(request.getParameter("segundo")) == 0
+//						|| Integer.parseInt(request.getParameter("segundo")) < 0) {
+//					lineaerrores = new ArrayList<String>();
+//					lineaerrores.add("Error: Introduce un valor mayor que 0 en segundo");
+//					errores.add(lineaerrores);
+//				} else {
+//					lineaerrores = new ArrayList<String>();
+//					lineaerrores.add("Error: Introduce un valor menor que 101 en segundo");
+//					errores.add(lineaerrores);
+//				}
+//			}
+//			if (errores.isEmpty() == false) {
+//				response.setStatus(404);
+//				Gson gSon = new Gson();
+//				String str = gSon.toJson(errores);
+//				response.getWriter().write(str);
+//
+//			} else {
+//
+//				// SI HA PASADO LAS VALIDACIONES DE ARRIBA QUIERE DECIR QUE LO QUE HE
+//				// INTRODUCIDO SIN NUMEROS
+//				// CORRECTOS ASI QUE CREARA LA TABLA
+//				Gson gSon = new Gson();
+//				String str = "";
+//				
+//				Double primero = Double.parseDouble(request.getParameter("primero"));
+//				Double segundo = Double.parseDouble(request.getParameter("segundo"));
+//
+//				Double resultado = 0.0;
+//
+//				BigDecimal value = new BigDecimal(0);
+//
+//				arrayResultado = new ArrayList();
+//
+//				switch (accion) {
+//				case "Suma":
+//					resultado = primero + segundo;
+//					arrayResultado.add(resultado);
+//					str = gSon.toJson(arrayResultado);
+//					response.getWriter().append(str);
+//					break;
+//				case "Resta":
+//					resultado = primero - segundo;
+//					arrayResultado.add(resultado);
+//					str = gSon.toJson(arrayResultado);
+//					response.getWriter().append(str);
+//					break;
+//				case "Multiplicacion":
+//					resultado = primero * segundo;
+//					arrayResultado.add(resultado);
+//					str = gSon.toJson(arrayResultado);
+//					response.getWriter().append(str);
+//					break;
+//				case "Division":
+//					resultado = primero / segundo;
+//					arrayResultado.add(resultado);
+//					str = gSon.toJson(arrayResultado);
+//					response.getWriter().append(str);
+//					break;
+//				default:
+//					response.setStatus(404);
+//					lineaerrores = new ArrayList<String>();
+//					lineaerrores.add("Error: Selecciona una accion");
+//					errores.add(lineaerrores);
+//					str = gSon.toJson(errores);
+//					response.getWriter().write(str);
+//					break;
+//				}
+//			}
+//		}
 
 	}
 
